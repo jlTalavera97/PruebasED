31ed182e3532397c0c78574ca16c0078fd884653
jlTalavera97 <joseluistalavera974@gmail.com>
2021-02-23 20:39:15 +0100

    Added tests in Persona

--- src/ed/examen/modelo/test/PersonaTest.java
+++ src/ed/examen/modelo/test/PersonaTest.java
@@ -4,41 +4,67 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import ed.examen.modelo.Persona;
+
 class PersonaTest {
+	Persona p = new Persona("73452419X", "Jose Luis", "Talavera");
 
 	@Test
 	void testPersonaStringStringString() {
-		fail("Not yet implemented");
+		Persona p2 = new Persona("12345678Y", "Pepe", "Martinez");
+
+		assertEquals("12345678Y", p2.getDni());
+		assertEquals("Pepe", p2.getNombre());
+		assertEquals("Martinez", p2.getApellido1());
 	}
 
 	@Test
 	void testGetDni() {
-		fail("Not yet implemented");
+		assertEquals("73452419X", p.getDni());
 	}
 
 	@Test
 	void testSetDni() {
-		fail("Not yet implemented");
+
+		// Valid catch
+		try {
+			p.setDni("12345678P");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals("12345678P", p.getDni());
+
+		// Invalid class
+		boolean launchedException = false;
+		try {
+			p.setDni("12345678");
+		} catch (Exception e) {
+			launchedException = true;
+		}
+		assertTrue(launchedException);
+
 	}
 
 	@Test
 	void testGetNombre() {
-		fail("Not yet implemented");
+		assertEquals("Jose Luis", p.getNombre());
 	}
 
 	@Test
 	void testSetNombre() {
-		fail("Not yet implemented");
+		p.setNombre("Carlos");
+		assertEquals("Carlos", p.getNombre());
 	}
 
 	@Test
 	void testGetApellido1() {
-		fail("Not yet implemented");
+		assertEquals("Talavera", p.getApellido1());
 	}
 
 	@Test
 	void testSetApellido1() {
-		fail("Not yet implemented");
+		p.setApellido1("Saenz");
+		assertEquals("Saenz", p.getApellido1());
 	}
 
 }
